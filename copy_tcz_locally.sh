765581ffbd2d9fc1e4c56f4ac29a46abe57ef6b1
linic <linic@hotmail.ca>
2024-10-12 09:46:01 -0400

    Rust 1.81.0 update and some improvements.

--- copy_tcz_locally.sh
+++ copy_tcz_locally.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # NOTE: you may need to call this script with sudo unless you are root.
 id=$(docker create linichotmailca/tcl-core-rust-i586:latest)
-docker cp $id:/home/tc/rust-1.80.0-i586.tcz .
+docker cp $id:/home/tc/$latest_rust_tcz .
 docker rm -v $id
